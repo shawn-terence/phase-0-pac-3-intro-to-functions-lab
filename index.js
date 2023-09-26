6637d8d20cb7ddcd74ae1cd92f789ac88b6eaa1d
terence <terenceshawn18@outlook.com>
2023-09-26 18:23:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+  }
+  function whisper(string){
+    return string.toLowerCase();
+  }
+
+  function logShout(string){
+    console.log(string.toUpperCase());
+  }
+  function logWhisper(string){
+    console.log(string.toLowerCase());
+  }
+ function sayHiToHeadphonedRoommate(string){
+    console.log(string.toLowerCase())
+    return `I can\'t hear you!`
+ }
+
+ function sayHiToHeadphonedRoommate(string) {
+    if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else {
+      return "I can't hear you!";
+    }
+  }
+  
+
+  
